f865eac3cffac05ae01df9950e824e773f7cc35d
Sydney Pickle <sydneyrpickle99@gmail.com>
2021-03-03 21:01:10 -0600

    End of Video #1

--- explore_json_2.py
+++ explore_json_2.py
@@ -27,7 +27,20 @@ print(lats[:10])
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "marker": {
+            "size": [5 * mag for mag in mags],
+            "color": mags,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Magnitude"},
+        },
+    }
+]
 
 my_layout = Layout(title="Global Earthquakes")
 
--- explore_json_3.py
+++ explore_json_3.py
@@ -9,15 +9,18 @@ json.dump(eq_data, outfile, indent=4)
 
 list_of_eqs = eq_data["features"]
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 
 for eq in list_of_eqs:
     mag = eq["properties"]["mag"]
     lon = eq["geometry"]["coordinates"][0]
     lat = eq["geometry"]["coordinates"][1]
+    hover_text = eq["properties"]["place"]
+
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(hover_text)
 
 print(mags[:10])
 print(lons[:10])
@@ -27,7 +30,21 @@ print(lats[:10])
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "text": hover_texts,
+        "marker": {
+            "size": [5 * mag for mag in mags],
+            "color": mags,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Magnitude"},
+        },
+    }
+]
 
 my_layout = Layout(title="Global Earthquakes")
 
